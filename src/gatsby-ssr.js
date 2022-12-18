6218ece78621b4dfd03f3056b950c7e81dfd2c68
Berkay <berkaydemirbas@yahoo.com>
2022-12-19 00:21:18 +0300

    added eventModel and builtins

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -12,7 +12,9 @@ exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
     src: pluginOptions.srcUrl,
     "data-website-id": pluginOptions.websiteId,
     "data-auto-track": 'autoTrack' in pluginOptions ? pluginOptions.autoTrack : true,
-    "data-do-not-track": 'respectDoNotTrack' in pluginOptions ? pluginOptions.respectDoNotTrack : true
+    "data-do-not-track": 'respectDoNotTrack' in pluginOptions ? pluginOptions.respectDoNotTrack : true,
+    "built-in-events": 'builtInEvents' in pluginOptions ? pluginOptions.builtInEvents : true,
+    "eventModel": 'eventModel' in pluginOptions ? pluginOptions.eventModel : 'web2'
   }
 
   return setPostBodyComponents([<script key="gastby-plugin-hardal" async defer {...options} />])
